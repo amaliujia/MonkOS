948c8e67c8ca98a9989e5ba8d154381fd8526179
Rui Wang <amaliujia@163.com>
2014-03-09 16:52:49 +0800

    clean up makefile and redesign os structure

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+OBJS_BOOTPACK = bootpack.obj naskfunc.obj hankaku.obj graphic.obj dsctbl.obj
+
 TOOLPATH = ../z_tools/
 INCPATH  = ../z_tools/haribote/
 
@@ -5,9 +7,9 @@ MAKE     = $(TOOLPATH)make.exe -r
 NASK     = $(TOOLPATH)nask.exe
 CC1      = $(TOOLPATH)cc1.exe -I$(INCPATH) -Os -Wall -quiet
 GAS2NASK = $(TOOLPATH)gas2nask.exe -a
+OBJ2BIM  = $(TOOLPATH)obj2bim.exe
 MAKEFONT = $(TOOLPATH)makefont.exe
 BIN2OBJ  = $(TOOLPATH)bin2obj.exe
-OBJ2BIM  = $(TOOLPATH)obj2bim.exe
 BIM2HRB  = $(TOOLPATH)bim2hrb.exe
 RULEFILE = $(TOOLPATH)haribote/haribote.rul
 EDIMG    = $(TOOLPATH)edimg.exe
@@ -15,28 +17,17 @@ IMGTOL   = $(TOOLPATH)imgtol.com
 COPY     = copy
 DEL      = del
 
-# make
+
 default :
 	$(MAKE) img
 
-# make img
+
+
 ipl10.bin : ipl10.nas Makefile
 	$(NASK) ipl10.nas ipl10.bin ipl10.lst
 
 asmhead.bin : asmhead.nas Makefile
 	$(NASK) asmhead.nas asmhead.bin asmhead.lst
-
-bootpack.gas : bootpack.c Makefile
-	$(CC1) -o bootpack.gas bootpack.c
-
-bootpack.nas : bootpack.gas Makefile
-	$(GAS2NASK) bootpack.gas bootpack.nas
-
-bootpack.obj : bootpack.nas Makefile
-	$(NASK) bootpack.nas bootpack.obj bootpack.lst
-
-naskfunc.obj : naskfunc.nas Makefile
-	$(NASK) naskfunc.nas naskfunc.obj naskfunc.lst
 
 hankaku.bin : hankaku.txt Makefile
 	$(MAKEFONT) hankaku.txt hankaku.bin
@@ -44,46 +35,54 @@ hankaku.bin : hankaku.txt Makefile
 hankaku.obj : hankaku.bin Makefile
 	$(BIN2OBJ) hankaku.bin hankaku.obj _hankaku
 
-bootpack.bim : bootpack.obj naskfunc.obj hankaku.obj Makefile
+bootpack.bim : $(OBJS_BOOTPACK) Makefile
 	$(OBJ2BIM) @$(RULEFILE) out:bootpack.bim stack:3136k map:bootpack.map \
-		bootpack.obj naskfunc.obj hankaku.obj
+		$(OBJS_BOOTPACK)
+# 3MB+64KB=3136KB
 
 bootpack.hrb : bootpack.bim Makefile
 	$(BIM2HRB) bootpack.bim bootpack.hrb 0
 
-haribote.sys : bootpack.hrb asmhead.bin Makefile
+haribote.sys : asmhead.bin bootpack.hrb Makefile
 	copy /B asmhead.bin+bootpack.hrb haribote.sys
 
-MonkOS.img : ipl10.bin haribote.sys Makefile
+haribote.img : ipl10.bin haribote.sys Makefile
 	$(EDIMG)   imgin:../z_tools/fdimg0at.tek \
-		wbinimg src:ipl10.bin len:512 from:0 to:0   \
-		copy from:haribote.sys to:@:	\
-		imgout:MonkOS.img
+		wbinimg src:ipl10.bin len:512 from:0 to:0 \
+		copy from:haribote.sys to:@: \
+		imgout:haribote.img
 
-# milestones
+%.gas : %.c Makefile
+	$(CC1) -o $*.gas $*.c
+
+%.nas : %.gas Makefile
+	$(GAS2NASK) $*.gas $*.nas
+
+%.obj : %.nas Makefile
+	$(NASK) $*.nas $*.obj $*.lst
+
+
 img :
-	$(MAKE) MonkOS.img
+	$(MAKE) haribote.img
 
 run :
 	$(MAKE) img
-	$(COPY) MonkOS.img ..\z_tools\qemu\fdimage0.bin
+	$(COPY) haribote.img ..\z_tools\qemu\fdimage0.bin
 	$(MAKE) -C ../z_tools/qemu
 
 install :
 	$(MAKE) img
-	$(IMGTOL) w a: MonkOS.img
+	$(IMGTOL) w a: haribote.img
 
 clean :
 	-$(DEL) *.bin
 	-$(DEL) *.lst
-	-$(DEL) haribote.sys
-	-$(DEL) *.gas
-	-$(DEL) bootpack.nas
 	-$(DEL) *.obj
+	-$(DEL) bootpack.map
 	-$(DEL) bootpack.bim
 	-$(DEL) bootpack.hrb
-	-$(DEL) bootpack.map
+	-$(DEL) haribote.sys
 
 src_only :
 	$(MAKE) clean
-	-$(DEL) MonkOS.img
+	-$(DEL) haribote.img
---
+++ graphic.c
@@ -0,0 +1,159 @@
+#include "bootpack.h"
+
+void init_color()
+{
+    static unsigned char color_table[16 * 3]={
+    	0x00, 0x00, 0x00,
+    	0xff, 0x00, 0x00,
+    	0x00, 0xff, 0x00,
+    	0xff, 0xff, 0x00,
+    	0x00, 0x00, 0xff,
+    	0xff, 0x00, 0xff,
+    	0x00, 0xff, 0xff,
+    	0xff, 0xff, 0xff,
+    	0xc6, 0xc6, 0xc6,
+    	0x84, 0x00, 0x00,
+    	0x00, 0x84, 0x00,
+    	0x84, 0x84, 0x00,
+    	0x00, 0x00, 0x84,
+    	0x84, 0x00, 0x84,
+    	0x00, 0x84, 0x84,
+    	0x84, 0x84, 0x84
+    };
+    set_color(0, 15, color_table);
+ 	return;   
+}
+
+void set_color(int start, int end, unsigned char *rgb)
+{
+	int i, eflags;
+	eflags = io_load_eflags();
+	io_cli();
+
+	//指向正确的色号
+	rgb += (start * 3);
+
+	//0x03c8, port, 指向video DA Converter 地址寄存器
+	io_out8(0x03c8, start);
+	for (i = start; i < end; ++i)
+	{
+		io_out8(0x03c9, rgb[0]/4);
+		io_out8(0x03c9, rgb[1]/4);
+		io_out8(0x03c9, rgb[2]/4);
+		rgb += 3;
+	}
+	io_store_eflags(eflags);
+	return;
+}
+
+void draw_box8(unsigned char *vram, int xsize, unsigned char c, int xs, int ys, int xe, int ye)
+{
+	int x, y;
+    for(y = ys; y <= ye; y++)
+    {
+    	for (x = xs; x <= xe;x++)
+    	{
+    		vram[x + y * xsize] = c;
+    	}
+    }
+    return;
+}
+
+
+void put_font8(unsigned char *vram, int xsize, unsigned char fontColor, char *c, int x, int y)
+{
+	int yo;
+	int i = 0;
+	char d;
+	for (yo = y; yo < y+16; ++yo)
+	{
+		d = c[i];
+		if ((d & 0x80) != 0) {vram[yo * xsize + x + 0] = fontColor;}
+		if ((d & 0x40) != 0) {vram[yo * xsize + x + 1] = fontColor;}
+		if ((d & 0x20) != 0) {vram[yo * xsize + x + 2] = fontColor;}
+		if ((d & 0x10) != 0) {vram[yo * xsize + x + 3] = fontColor;}
+		if ((d & 0x08) != 0) {vram[yo * xsize + x + 4] = fontColor;}
+		if ((d & 0x04) != 0) {vram[yo * xsize + x + 5] = fontColor;}
+		if ((d & 0x02) != 0) {vram[yo * xsize + x + 6] = fontColor;}
+		if ((d & 0x01) != 0) {vram[yo * xsize + x + 7] = fontColor;}
+		i++;
+	}
+	return;
+}
+
+void init_screen(char *vram, int xsize, int ysize)
+{
+	// 0xa0000 is start addr of VRAM. We wirte 1 byte(I guess int will be cut into 1 byte or 8 bits) into every addr, then we control all this screen
+	//draw wall paper
+	draw_box8(vram, xsize, COL8_008484,  0,         0,          xsize -  1, ysize);
+	// draw_box8(vram, xsize, COL8_C6C6C6,  0,         ysize - 28, xsize -  1, ysize - 28);
+	// draw_box8(vram, xsize, COL8_FFFFFF,  0,         ysize - 27, xsize -  1, ysize - 27);
+	// draw_box8(vram, xsize, COL8_C6C6C6,  0,         ysize - 26, xsize -  1, ysize -  1);
+	return;
+}
+
+void put_string8(unsigned char *vram, int xsize, unsigned char fontColor, char *str, int x, int y)
+{
+	extern char hankaku[4096];
+	char *temp;
+	int xt = x;
+	for (temp = str; *temp != 0x00; temp++)
+	{
+		put_font8(vram, xsize, fontColor, hankaku+(*temp)*16, xt, y);
+		xt += 8;
+	}
+	return;
+}
+
+void init_mouse_cursor8(char *mouse, char backgourdColor)
+{
+	static char cursor[16][16] = {
+		"**************..",
+		"*OOOOOOOOOOO*...",
+		"*OOOOOOOOOO*....",
+		"*OOOOOOOOO*.....",
+		"*OOOOOOOO*......",
+		"*OOOOOOO*.......",
+		"*OOOOOOO*.......",
+		"*OOOOOOOO*......",
+		"*OOOO**OOO*.....",
+		"*OOO*..*OOO*....",
+		"*OO*....*OOO*...",
+		"*O*......*OOO*..",
+		"**........*OOO*.",
+		"*..........*OOO*",
+		"............*OO*",
+		".............***"
+	};
+	int x, y;
+
+	for (y = 0; y < 16; y++) {
+		for (x = 0; x < 16; x++) {
+			if (cursor[y][x] == '*') {
+				mouse[y * 16 + x] = COL8_000000;
+			}
+			if (cursor[y][x] == 'O') {
+				mouse[y * 16 + x] = COL8_FFFFFF;
+			}
+			if (cursor[y][x] == '.') {
+				mouse[y * 16 + x] = backgourdColor;
+			}
+		}
+	}
+	return;
+
+}
+
+void draw_cursor(char *vram, int xsize, int cursorXSize, int cursorYSize, int startPointX, int startPointY, char *cursorBuf, int backgourdXSize)
+{
+	int x, y;
+	//int i = 0;
+	for (y = 0; y < cursorYSize; y++)
+	{
+		for (x = 0; x < cursorXSize; x++)
+		{
+			vram[(startPointY + y) * xsize + startPointX + x] = cursorBuf[y*backgourdXSize+x];
+		}
+	}
+	return;
+}
